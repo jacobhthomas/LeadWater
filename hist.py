b5cb88f9b2e8cc9ae5f7e80e07b9c468d86e0181
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2023-04-10 13:24:18 -0500

    histogrammy stuff

--- hist.py
+++ hist.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 
 
+df = pd.read_csv('datasets/assessorSequential.csv')
+df.drop(columns=['Unnamed: 0'])
+
+df['avg'] = df.filter(like='Draw').apply(lambda x: x.mean(), axis=1)
+df['interval'] = ['ppb < 5' if (x < 5) else '5 <= ppb < 15' if (x < 15) else '15 <= ppb' for x in df['avg']]
+
+# specify colors and labels of stacked bars
+colors=['navy', 'seagreen', 'darkred']
+names=['ppb < 5', '5 <= ppb < 15', '15 <= ppb']
+
+
+# #--------  ZIP CODE --------- 
+
 zips = [60645, 60626, 60631, 60646, 60659, 60660, 
 60656, 60630, 60625, 60640, 60634, 60641, 60618, 60613, 
 60657, 606707, 60739, 60647, 60614, 60651, 60622, 
@@ -21,42 +34,16 @@ def shorten_zip(zip):
         return np.NaN
     return zip[:5]
 
-df = pd.read_csv('datasets/assessorSequential.csv')
-df.drop(columns=['Unnamed: 0'])
 df['ZIP'] = df['ZIP'].apply(lambda x: shorten_zip(x))
 df = df[df['ZIP'].notna()]
 
 # calculate average lead level and which interval it is in 
-df['avg'] = df.filter(like='Draw').apply(lambda x: x.mean(), axis=1)
-df['interval'] = ['ppb < 5' if (x < 5) else '5 <= ppb < 15' if (x < 15) else '15 <= ppb' for x in df['avg']]
-
 df_zips = set(df['ZIP'])
 
 zip_list = [] 
 for z in zips: 
     if z in df_zips: 
         zip_list.append(z)
-
-# df.groupby(['ZIP']).mean()
-
-# # # filter out lead level intervals 
-# x1 = list(df[df['interval'] == 'ppb < 5']['ZIP'])
-# x2 = list(df[df['interval'] == '5 <= ppb < 15']['ZIP'])
-# x3 = list(df[df['interval'] == '15 <= ppb']['ZIP'])
-
-# specify colors and labels of stacked bars
-colors=['navy', 'seagreen', 'darkred']
-names=['ppb < 5', '5 <= ppb < 15', '15 <= ppb']
-
-# # plt.hist([x1, x2, x3], color=colors, label=names, density=True, stacked = True, rwidth=.9, bins=48, alpha = .7, align='mid')
-# # plt.title('Density of Average Lead Intervals by ZIP Code')
-# # plt.xlabel('ZIP Code')
-# # plt.ylabel('Density')
-# # plt.xticks(rotation=90, size=7)
-# # plt.yticks(size = 8)
-# # plt.legend(names, loc='upper right')
-
-# # plt.show()
 
 cross_tab_prop = pd.crosstab(index=df['ZIP'], columns=df['interval'], normalize='index')
 cross_tab =  pd.crosstab(index=df['ZIP'], columns=df['interval']) 
@@ -75,12 +62,65 @@ cross_tab=cross_tab.transpose()
 
 print(cross_tab)
 
-
 cross_tab_prop.plot(kind='bar', stacked=True, color=colors, label=names, alpha = .6, figsize=(10,6))
 plt.title('Proportion of Average Lead Intervals by ZIP Code')
 plt.xticks(size=7)
 plt.yticks(size = 8)
 plt.ylabel('Proportion')
+plt.legend(names, loc='upper right')
+
+plt.show() 
+
+cross_tab.plot(kind='bar', stacked=True, color=colors, label=names, alpha = .6, figsize=(10,6))
+plt.title('Frequency of Average Lead Intervals by ZIP Code')
+plt.xticks(size=7)
+plt.yticks(size = 8)
+plt.ylabel('Frequency')
+plt.legend(names, loc='upper right')
+
+plt.show() 
+
+# --------- Sale Price --------- # 
+# df = df[df['Sale Price'].notna()]
+# df['Sale Price'] = df['Sale Price'].apply(lambda x: round(x / 10000.0) * 10000.0)
+
+# cross_tab_prop = pd.crosstab(index=df['Sale Price'], columns=df['interval'], normalize='index')
+# cross_tab =  pd.crosstab(index=df['Sale Price'], columns=df['interval']) 
+# print(cross_tab)
+
+
+# cross_tab_prop.plot(kind='bar', stacked=True, color=colors, label=names, alpha = .6, figsize=(10,6))
+# plt.title('Proportion of Average Lead Intervals by ZIP Code')
+# plt.xticks(size=7)
+# plt.yticks(size = 8)
+# plt.ylabel('Proportion')
+# plt.legend(names, loc='upper right')
+
+# plt.show() 
+
+
+df = df[df['Tract Median Income'].notna()]
+df['Tract Median Income'] = df['Tract Median Income'].apply(lambda x: round(x / 10000.0) * 10000.0)
+
+cross_tab_prop = pd.crosstab(index=df['Tract Median Income'], columns=df['interval'], normalize='index')
+cross_tab =  pd.crosstab(index=df['Tract Median Income'], columns=df['interval']) 
+print(cross_tab)
+
+
+cross_tab_prop.plot(kind='bar', stacked=True, color=colors, label=names, alpha = .6, figsize=(10,6))
+plt.title('Proportion of Average Lead Intervals by Tract Median Income')
+plt.xticks(size=7)
+plt.yticks(size = 8)
+plt.ylabel('Proportion')
+plt.xlabel('Median Tract Income')
+plt.legend(names, loc='upper right')
+
+cross_tab.plot(kind='bar', stacked=True, color=colors, label=names, alpha = .6, figsize=(10,6))
+plt.title('Frequency of Average Lead Intervals by Tract Median Income')
+plt.xticks(size=7)
+plt.yticks(size = 8)
+plt.ylabel('Frequency')
+plt.xlabel('Median Tract Income')
 plt.legend(names, loc='upper right')
 
 plt.show() 
